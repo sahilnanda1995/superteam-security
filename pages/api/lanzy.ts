9d88fd8b7310b61d8779f3f576255b69fb95747d
Deep Mehta <itsdeveloperdeep@gmail.com>
2023-03-16 13:58:58 +0530

    vercel fs sukcs

--- pages/api/lanzy.ts
+++ pages/api/lanzy.ts
@@ -4,35 +4,35 @@ import path from "path";
 import archiver from "archiver";
 import fs from "fs";
 
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const dir = path.resolve("ctfs/lanzy");
-try {
-
-  if (req.method === 'POST') {
-    let parseBody = JSON.parse(req.body);
-    
-    fs.writeFileSync(`${dir}/keys/pubkeys.json`, JSON.stringify(parseBody));
-    res.status(200).json({ status: "Done" });
-
-  } else if (req.method === 'GET') {
-    res.setHeader("Content-Type", "application/zip");
-    res.setHeader("Content-Disposition", "attachment; filename=lanzy.zip");
-
-    const zip = archiver("zip");
-    
-    zip.on('error', function(err) {
-      throw err;
-    });
-    zip.pipe(res);
-    zip.directory(dir, "lanzy");
-    zip.finalize();
-  }  
-} catch (e) {
-  console.log(`error :: ${e}`);
+  const dir = path.resolve("public/ctfs/lanzy");
+  console.log("dir :: ", dir);
   
-}
+  try {
+    if (req.method === "POST") {
+      console.log("seralizing ");
+      
+      let parseBody = JSON.parse(req.body);
+      fs.writeFileSync(`${dir}/keys/pubkeys.json`, JSON.stringify(parseBody));
+
+      res.status(200).json({ status: "Done" });
+    } else if (req.method === "GET") {
+      res.setHeader("Content-Type", "application/zip");
+      res.setHeader("Content-Disposition", "attachment; filename=lanzy.zip");
+
+      const zip = archiver("zip");
+
+      zip.on("error", function (err) {
+        throw err;
+      });
+      zip.pipe(res);
+      zip.directory(dir, "lanzy");
+      zip.finalize();
+    }
+  } catch (e) {
+    console.log(`error :: ${e}`);
+  }
 }
